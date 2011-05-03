847aa73e0417bc498c82753cdccc974f8875c134
Andrew Burgess <andrew8088@gmail.com>
2011-05-03 18:01:00 -0400

    post.title property working

--- post.rb
+++ post.rb
@@ -2,7 +2,7 @@ require 'maruku'
 require 'date'
 
 class Post
-  attr_accessor :date, :tags
+  attr_accessor :date, :tags, :title
   # file_path = path to file
   # should end with filename like this:
   # yyyy-mm-dd-my_descript_with_underscores-tag-multiword_tag-tag
@@ -13,6 +13,12 @@ class Post
       @date, descript, @tags = md.to_a
       @date = Date.parse @date
       @tags = @tags.split "-"
+    end
+
+    File.open(file_path) do |file|
+      begin
+        @title = file.readline
+      end until @title.match(/^#+(.*)/)
     end
   end
 end
--- tests/post_test.rb
+++ tests/post_test.rb
@@ -3,7 +3,7 @@ require_relative '../post'
 
 class PostText < Test::Unit::TestCase
 
-  @@filename = "some/path/to/2011-05-03-this_is_a_descript-tag-another_tag-awesome.txt"
+  @@filename = File.join Dir.pwd, "tests", "2011-05-03-sample_post-tag-another_tag-final_tag.txt"
   @@post     = Post.new @@filename
 
   def test_date
@@ -20,5 +20,13 @@ class PostText < Test::Unit::TestCase
 
   def test_tags_property_is_array_object
     assert @@post.tags.is_a?(Array), "tags property is an array"
+  end
+
+  def test_post_title
+    assert defined? @@post.title, "title property defined"
+  end
+
+  def test_post_title_is_a_string
+    assert @@post.title.is_a?(String), "title property is a string"
   end
 end
